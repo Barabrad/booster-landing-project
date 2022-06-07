ad46a27c1eb62c389be4886a50660071e64e32a2
Brad Barakat <78241382+Barabrad@users.noreply.github.com>
2022-06-07 10:42:23 -0700

    Add files via upload

---
+++ ControlBooster.m
@@ -0,0 +1,156 @@
+function [angAtt, phase, mdot, Fx] = ControlBooster(t, DSuicide, state, parameters)
+    global p4Y time2 time3 oldMdot oldFx;
+
+    g = 9.8;
+
+    h = parameters(1);
+    D = parameters(2);
+    Ms = parameters(3);
+    Isp = parameters(4);
+    mdotMax = parameters(5);
+    FxMax = parameters(6);
+
+    x = state(1);
+    y = state(2);
+    Vx = state(3);
+    Vy = state(4);
+    theta = state(5);
+    omega = state(6);
+    M = state(7);
+    I = rocketMoment(M);
+    
+    ueq = Isp * g;
+    
+    %Take cos then acos to "synchronise" angles
+    angleR = cosd(theta - 180); %Angle of the rocket relative to vertical
+    angleP = cosd(90 - atan2d(Vy, Vx)); %Angle of path rel. to ver.
+    angAtt = acosd(angleR) - acosd(angleP);
+    
+    if abs(Vx) < 10
+        thetaLQR = theta; %Goal: zero out theta
+    else
+        thetaLQR = angAtt; %Zeroing out angle of attack reduces Vx
+    end
+    
+    function I = rocketMoment( M )
+        I = (M/12) * ( 3*(D/2)^2 + h^2 );
+    end
+
+    function Fx = CorrectAngle(angleDiff, omega)
+        LQRs3 = [ angleDiff, omega ]';
+
+        A = [ 0 1;...
+            0 0 ];
+
+        B = [ 0 (0.5*h/I)*(180/pi) ]';
+        
+        Q = diag( [ 2 5 ] );
+
+        R = 1e-6;
+        N = 0;
+
+        k = lqr( A, B, Q, R, N );
+        u = -k * LQRs3;
+
+        Fx = u;
+    end
+
+% Phase 1: End-over-end
+    tEOEhalf = ceil(sqrt( 2*pi*I/(FxMax*h) ));
+    if t < tEOEhalf
+        phase = "End-over-end (a)";
+        mdot = 0;
+        Fx = FxMax;
+        return
+    elseif (t >= tEOEhalf && t < 2*tEOEhalf)
+        phase = "End-over-end (d)";
+        mdot = 0;
+        Fx = -FxMax;
+        return
+    elseif (t >= 2*tEOEhalf && t < 2*tEOEhalf + 0.2)
+        phase = "Stopping Fx";
+        mdot = 0;
+        Fx = 0;
+        return
+    end
+    
+% Phase 2: Coast
+    if (y > DSuicide)
+        phase = "Coasting";
+        mdot = -x/6300; %A small arbitrary force to reduce overshot of x=0
+        Fx = CorrectAngle(thetaLQR, omega);
+        time2 = t;
+        time3 = t;
+    else
+% Phase 3: Decelerate
+        landingVy = -100;
+        if (Vy <= landingVy && time2 == time3)
+            phase = "Slowing";
+            mdot = mdotMax;
+            Fx = CorrectAngle(thetaLQR, omega);
+            time2 = t;
+            time3 = t;
+        elseif (Vy > -1)
+            phase = "Landing";
+            mdot = 0;
+            Fx = CorrectAngle(thetaLQR, omega);
+        else
+% Phase 4: Landing
+            if time2 == time3
+                p4Y = y; %The height of switch from dec. to landing
+            end
+            phase = "Landing";
+            %We need a transition from Vy0 to Vy = -1
+            buffer = 30;
+            if y > buffer
+                VDesired = landingVy + (1-landingVy)*(p4Y-y)/(p4Y-buffer);
+            else
+                VDesired = -1;
+            end
+            
+            LQRs2 = [ x, Vx, thetaLQR, omega, y, Vy-VDesired ]';
+            
+            if oldMdot <= 0
+                Fth0 = M*g;
+            else
+                Fth0 = oldMdot*ueq;
+            end
+
+            A = [ 0 1 0 0 0 0;...
+                0 0 (Fth0/M)*(pi/180) 0 0 0;...
+                0 0 0 1 0 0;...
+                0 0 0 0 0 0;...
+                0 0 0 0 0 1;...
+                0 0 (oldFx/M)*(pi/180) 0 0 0];
+
+            B = [ 0 (1/M) 0 (0.5*h/I)*(180/pi) 0 0;...
+                0 0 0 0 0 (ueq/M)]';
+
+            Q = diag( [ 1 2 5 10 1 5 ] );
+
+            R = [1e-6 0;...
+                0 1e-6];
+            N = [0 0; 0 0; 0 0; 0 0; 0 0; 0 0];
+
+            k = lqr( A, B, Q, R, N );
+            u = -k * LQRs2;
+
+            Fx = u(1);
+            
+            if u(2) < 0
+                mdot = M/Isp; %keeps Vy constant
+            else
+                mdot = u(2) + M/Isp;
+            end
+            
+            if (Vy < landingVy-50) %Switch back to dec. if speed increases
+                time2 = t;
+                time3 = t;
+            else
+                time3 = t;
+            end
+        end
+    end
+    oldMdot = mdot;
+    oldFx = Fx;
+end
---
+++ simulateBooster.m
@@ -0,0 +1,188 @@
+function simulateBooster(controlBooster)
+% Main program for running booster landing simulation
+
+    if nargin < 1
+        controlBooster = @ControlBooster;
+    end
+
+    % Physical quantities
+    g = 9.8; % Earth gravity (constant, m/s^2)
+    km = 1000; 
+
+    % Assumed quantities
+    h = 50; % Height of booster, m
+    D = 5; % Diameter of booster, m
+    Ms = 1e4; % Empty (structural) mass of booster, kg
+    R = 12; % Mass ratio after BECO
+    Isp = 350; % Specific impulse of rocket engines
+    alphaMax0 = 5; % Max. angular acceleration (deg/s^2) after BECO
+    accelMax0 = 2*g; % Acceleration at maximum mdot after BECO
+    mdotError = 0.01; % Commanded mass flow has this much fractional error
+    FxError = 0.05; % Commanded sideways force has this much fractional error
+
+    % For computation
+    dt = 0.1; % Timestep
+
+    % Derived quantities
+    Mp0 = (R - 1) * Ms; % Propellant mass after BECO
+    M0 = Ms + Mp0; % Rocket mass after BECO
+    mdotMax = accelMax0 / (Isp*g) * R*Ms;
+    I0 = (M0/12) * ( 3*(D/2)^2 + h^2 ); % Initial moment of inertia
+    % Fx h/2 = I0 * alpha
+    FxMax = I0 * alphaMax0 / (h/2) * pi/180;
+    ueq = Isp*g;
+
+    % Initial conditions
+    % Per https://www.reddit.com/r/spacex/comments/bcdss4/some_meco_and_beco_speeds/
+    % BECO cuts off 1611 m/s, altitude 58 km, T=02:35
+    x0 = -200 * km;
+    y0 = 60 * km;
+    V0 = 1600;
+    theta0 = 80;
+    omega0 = 0;
+    Vx0 = V0 * sind(theta0);
+    Vy0 = V0 * cosd(theta0);
+
+    state0 = [ x0 y0 Vx0 Vy0 theta0 omega0 M0 ]';
+    state = state0;
+
+    parameters = [ h, D, Ms, Isp, mdotMax, FxMax ];
+
+    t = 0;
+
+    function I = rocketMoment(M)
+        I = (M/12) * ( 3*(D/2)^2 + h^2 );
+    end
+
+    function q = addError(qNominal, fractionalError)
+        mu = qNominal;
+        sigma = abs(fractionalError * mu);
+        q = normrnd(mu, sigma);
+    end
+
+    %Found this on Desmos, but I added the buffer multiplier
+    function D = suicideBurn(Vy, M, g, y, ueq, mdot, bufferMult)
+        T = M*(1-exp( (Vy - sqrt(2*g*y))/(ueq) ))/mdot;
+        D = (T/2)*(-Vy + sqrt(2*g*y))*bufferMult;
+    end
+
+    close all;
+    figure;
+
+    %Height at which to start suicide burn
+    DSuicide = suicideBurn(Vy0, M0, g, y0, ueq, mdotMax, 3);
+
+    % Main simulation loop
+    i = 1;
+    fprintf( 'Initial: t=%f, x=%f, y=%f, Vx=%f, Vy=%f, theta=%f, omega=%f\n', ...
+        t, x0, y0, Vx0, Vy0, theta0, omega0 );
+    while( state(2) > 0 && t < 1000 )
+        [angAtt, phase, mdot, Fx] = controlBooster(t, DSuicide, state, parameters);
+        Fx = max( -FxMax, min( FxMax, Fx ) ); % Ensure legal value
+        Fx = addError( Fx, FxError ); % Add noise
+        x = state(1);
+        y = state(2);
+        Vx = state(3);
+        Vy = state(4);
+        theta = -180 + mod( state(5)+180, 360 );
+        omega = state(6);
+        M = state(7);
+        I = rocketMoment(M);
+        if( M > Ms )
+            mdot = max( 0, min( mdotMax, mdot ) ); % Ensure legal value
+            mdot = addError(mdot, mdotError); % Add noise
+            accel = mdot*ueq / M;
+            ax = accel*sind(theta) + Fx*cosd(theta)/M;
+            ay = accel*cosd(theta) - Fx*sind(theta)/M;
+            Vx = Vx + ax*dt;
+            Vy = Vy + ay*dt;
+            M = M - mdot*dt;
+            if M <= Ms
+                fprintf("/nFuel spent!/n");
+            end
+        else
+            mdot = 0;
+            Fx = 0;
+        end
+        Vy = Vy - g*dt; % Include gravity
+        x = x + Vx*dt;
+        y = y + Vy*dt;
+
+        alpha = Fx * h/2 / I * 180/pi;
+        theta = theta + omega*dt;
+        omega = omega + alpha*dt;
+
+        state = [ x, y, Vx, Vy, theta, omega, M ]';
+
+        tvals(i) = t;
+        xvals(i) = x;
+        yvals(i) = y;
+        Vxvals(i) = Vx;
+        Vyvals(i) = Vy;
+        thetavals(i) = theta;
+        omegavals(i) = omega;
+        attackvals(i) = angAtt;
+        Mvals(i) = M;
+
+        t = t + dt;
+        i = i + 1;
+
+        fprintf( '%s: mdot=%f, Fx=%f, t=%f, x=%f, y=%f, Vx=%f, Vy=%f, theta=%f, omega=%f\n', ...
+            phase, mdot, Fx, t, x, y, Vx, Vy, theta, omega );
+
+    end
+    x = state(1);
+    y = state(2);
+    Vx = state(3);
+    Vy = state(4);
+    theta = state(5);
+    omega = state(6);
+    
+    if (Vy < -5 || abs(Vx) > 2 || abs(theta) > 5 || abs(omega) > 10)
+        keyTerm = "crashed";
+    else
+        keyTerm = "landed";
+    end
+
+    fprintf( '\nBooster %s at t=%f, x=%f, Vx=%f, Vy=%f, theta=%f, omega=%f\n', ...
+        keyTerm, t, x, Vx, Vy, theta, omega );
+    fprintf( 'Propellant Used: %f kg out of %f kg\n', M0 - M, Mp0 );
+
+    subplot(4,2,1);
+    plot(tvals, xvals);
+    ylabel( 'x, m' );
+
+    subplot(4,2,2);
+    plot(tvals, yvals);
+    ylabel( 'y, m' );
+
+    subplot(4,2,3);
+    plot(tvals, Vxvals);
+    ylabel( 'Vx, m/s' );
+
+    subplot(4,2,4);
+    plot(tvals, Vyvals);
+    ylabel( 'Vy, m/s' );
+
+    subplot(4,2,5);
+    plot(tvals, thetavals);
+    ylabel( '\theta, degrees' );
+
+    subplot(4,2,6);
+    plot(tvals, attackvals);
+    ylabel( 'Angle of attack, degrees' );
+    
+    subplot(4,2,7);
+    plot(tvals, omegavals);
+    ylabel( '\omega, degrees/s' );
+
+    subplot(4,2,8);
+    plot(tvals, Mvals);
+    ylabel( 'Rocket mass M, kg' );
+    xlabel( 'Time, s' );
+
+end
+  
+  
+  
+
